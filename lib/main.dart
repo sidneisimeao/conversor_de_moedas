600ba25509c88faacdeb346ccc4fb49901ab44da
Sidnei Simmon <sidneisimmon@gmail.com>
2019-07-21 11:32:12 -0300

    ajustes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() => MaterialApp();
+import 'package:http/http.dart' as http;
+import 'package:async/async.dart';
+import 'dart:convert';
+
+const request = "https://api.hgbrasil.com/finance?key=5be23b20";
+
+void main() async {
+  http.Response response = await http.get(request);
+
+  print(json.decode(response.body)["results"]);
+
+  runApp(MaterialApp(
+    home: Container(),
+  ));
+}
